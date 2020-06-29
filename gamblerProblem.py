341c3c97f414db2ffe534d585886f7507844a406
RobinC94 <sissure@qq.com>
2020-06-29 12:11:29 +0800

    update value iterating

--- gamblerProblem.py
+++ gamblerProblem.py
@@ -27,50 +27,27 @@ V = np.zeros((101,))
 Action = np.zeros((101,))
 
 
-def policy_evaluate():
+def policy_action_evaluate():
     delta = 0
-    for i in range(101):
+    for i in range(100):
         v = V[i]
-        a = Action[i]
-        V[i] = value_calculate(i)
+        V[i], Action[i] = value_calculate(i)
         delta = max(delta, np.abs(v - V[i]))
     return delta
 
 
 def value_calculate(i):
     if i == 100:
-        return 1
-    temp_v = 0
+        return 0, 0
+    best_val = 0
+    best_act = 0
     i = int(i)
-    count = 0
     for a in range(min(i, 100-i)+1):
-        temp_v += ph * (R[i+a] + discount * V[i+a])
-        temp_v += (1 - ph) * (R[i-a] + discount * V[i-a])
-        count += 1
-    if count > 0:
-        temp_v /= count
-    return temp_v
-
-
-def action_greedy(i):
-    best_action = 0
-    best_value = 0
-    for a in range(min(i, 100-i)+1):
-        val = ph * V[i+a] + (1-ph) * V[i-a]
-        if val > best_value:
-            best_value = val
-            best_action = a
-    return best_action
-
-
-def policy_improve():
-    stable_flag = True
-    for i in range(101):
-        act_best = action_greedy(i)
-        if act_best != Action[i]:
-            Action[i] = act_best
-            stable_flag = False
-    return stable_flag
+        temp_v = ph * (R[i+a] + discount * V[i+a]) + (1 - ph) * (R[i-a] + discount * V[i-a])
+        if temp_v > best_val:
+            best_val = temp_v
+            best_act = a
+    return best_val, best_act
 
 
 def plot_value():
@@ -79,24 +56,25 @@ def plot_value():
     ax.plot(V)
 
 
+def plot_action():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, )
+    X = np.arange(101)
+    ax.bar(X, Action)
+
+
 if __name__ == '__main__':
     init_trans_prob()
-    stable = False
-    policies = []
-    count = 0
-    while not stable:
-        print("Evaluate Policies...")
-        while 1:
-            delta = policy_evaluate()
-            if delta == 0:
-                print("Evaluate Finished!")
-                break
-        print("Improve Policies...")
-        count += 1
-        stable = policy_improve()
-        policies.append(Action.copy())
+
+    print("Evaluate Policies...")
+    while 1:
+        delta = policy_action_evaluate()
         print(Action)
         print(V)
+        if delta == 0:
+            print("Evaluate Finished!")
+            break
     plot_value()
+    plot_action()
     plt.show()
 
